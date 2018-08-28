37e09ed40da4811f27d56737fc71086626b8d424
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-28 23:03:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ function BoardMember(name, homeState, training){
   this.veto = function() { return "No, I must disagree"};
   this.approve = function() { return "You can do that!";}
   this.doCharity = function() { return "I like to help people."};
+  this.releasePressStatement = function() { returns "You will see great things from Scuber."};
   this.sayHi = function() { returns `Hi, my name is${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;};
 }
